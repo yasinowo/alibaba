a5f69628cf7c4d87839edfa71ce3ce7208691b9c
yasinowo <zyzypwrmhmdyasyn@gmail.com>
2025-05-01 14:10:17 +0330

    on navigat

--- lib/components/navigationbar.dart
+++ lib/components/navigationbar.dart
@@ -1,8 +1,15 @@
+import 'package:alibaba/screen/home_page.dart';
+import 'package:alibaba/screen/mytravle_page.dart';
+import 'package:alibaba/screen/notifications_page.dart';
+import 'package:alibaba/screen/plus_page.dart';
+import 'package:alibaba/screen/profile_page.dart';
 import 'package:alibaba/theme/font.dart';
 import 'package:flutter/material.dart';
 
 class MyNavigationBar extends StatefulWidget {
-  const MyNavigationBar({super.key});
+  final ValueChanged<int>? onTabChange; // کال‌بک برای اطلاع رسانی تغییر تب
+
+  const MyNavigationBar({super.key, this.onTabChange});
 
   @override
   State<MyNavigationBar> createState() => _MyNavigationBarState();
@@ -10,18 +17,21 @@ class MyNavigationBar extends StatefulWidget {
 
 class _MyNavigationBarState extends State<MyNavigationBar> {
   int _currentIndex = 0;
-  final List<Widget> _pages = [
-    const Center(child: Text('صفحه خانه')),
-    const Center(child: Text('صفحه پلاس')),
-    const Center(child: Text('صفحه سفرهای من')),
-    const Center(child: Text('صفحه اعلان ها')),
-    const Center(child: Text('صفحه حساب کاربری')),
+  static final List<Widget> _pages = <Widget>[
+    HomePage(),
+    PlusPage(),
+    MytravlePage(),
+    NotificationsPage(),
+    ProfilePage(),
   ];
 
   void _onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
+    if (widget.onTabChange != null) {
+      widget.onTabChange!(index); // فراخوانی کال‌بک با اندیس تب انتخاب شده
+    }
   }
 
   @override
@@ -33,19 +43,19 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
         selectedLabelStyle: MyFonts.displaySmall,
         currentIndex: _currentIndex,
         onTap: _onTabTapped,
-        type: BottomNavigationBarType.fixed, // برای نمایش تمام عنوان‌ها
+        type: BottomNavigationBarType.fixed,
         items: [
           BottomNavigationBarItem(
             icon: Image.asset(
               'assets/icons/un_home.png',
-              width: 24, // تنظیم عرض دلخواه
-              height: 24, // تنظیم ارتفاع دلخواه
+              width: 24,
+              height: 24,
             ),
             activeIcon: Image.asset(
               'assets/icons/home.png',
-              color: Colors.blue, // مسیر عکس غیرفعال
-              width: 24, // تنظیم عرض دلخواه
-              height: 24, // تنظیم ارتفاع دلخواه
+              color: Colors.blue,
+              width: 24,
+              height: 24,
             ),
             label: 'خانه',
           ),
@@ -72,7 +82,6 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
               height: 24,
               color: Colors.blue,
             ),
-
             label: 'سفرهای من',
           ),
           BottomNavigationBarItem(
---
+++ lib/screen/profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:alibaba/components/defult_appbar.dart';
+import 'package:alibaba/components/navigationbar.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: defultAppbar(),
+      bottomNavigationBar: MyNavigationBar(),
+    );
+  }
+}
