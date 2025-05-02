7e2a53222aa5372b4fab1cc9821e3e5313dedb3a
yasinowo <zyzypwrmhmdyasyn@gmail.com>
2025-05-02 22:33:02 +0330

    update-visble nav

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -6,6 +6,7 @@ import 'package:alibaba/screen/notifications_page.dart';
 import 'package:alibaba/screen/plus_page.dart';
 import 'package:alibaba/screen/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -16,6 +17,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
+  bool _isNavBarVisible = true;
 
   final List<Widget> _pages = [
     const HomePage(),
@@ -31,14 +33,48 @@ class _MainScreenState extends State<MainScreen> {
     });
   }
 
+  void _handleScroll(ScrollNotification notification) {
+    if (notification is UserScrollNotification) {
+      final direction = notification.direction;
+      if (direction == ScrollDirection.forward && !_isNavBarVisible) {
+        setState(() => _isNavBarVisible = true);
+      } else if (direction == ScrollDirection.reverse && _isNavBarVisible) {
+        setState(() => _isNavBarVisible = false);
+      }
+    }
+  }
+
+  Widget _buildBody() {
+    Widget currentPage = _pages[_currentIndex];
+
+    // فقط برای Home و Plus
+    if (_currentIndex == 0 || _currentIndex == 1) {
+      return NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          _handleScroll(notification);
+          return false;
+        },
+        child: currentPage,
+      );
+    }
+
+    return currentPage;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: defultAppbar(),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: MyNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabTapped,
+      body: _buildBody(),
+      bottomNavigationBar: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        height: _isNavBarVisible ? kBottomNavigationBarHeight : 0,
+        child: Wrap(
+          // جلوگیری از overflow زمانی که height صفره
+          children: [
+            MyNavigationBar(currentIndex: _currentIndex, onTap: _onTabTapped),
+          ],
+        ),
       ),
     );
   }
